10be5a82280425283ffcbee05cb38216be068e08
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 11:04:14 +0000

    Adding tarfile member sanitization to extractall()

--- bus_identification_service/busid_inference.py
+++ bus_identification_service/busid_inference.py
@@ -64,7 +64,26 @@ class PythonPredictor:
 
     print("Extracting ", model_file)
     with tarfile.open(model_file) as buf:
-        buf.extractall(path=DOWNLOAD_DIR)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(buf, path=DOWNLOAD_DIR)
 
     # initiate graph to be used for inference
     self.detection_graph = tf.Graph()
